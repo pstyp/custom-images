c3cdcfef124e076a5c5f400210009b600e3691d7
Ubuntu <gumic@terVM.qcb5orqvbv1unpvuunb0d20nnd.zx.internal.cloudapp.net>
2020-07-24 14:54:57 +0000

    added files from friday (no systemctl)

--- script.sh
+++ script.sh
@@ -1,8 +1,7 @@
 az group create -n custom-images -l uksouth
-az vm availability-set create -g flask-app -l uksouth --platform-fault-domain-count 2 --platform-update-domain-count 5
-az vm create -g custom-images -n myVM -l uksouth --generate-ssh-key --image appImage
-ssh myVM -y
-git init
-git remote add origin https://github.com/pstyp/custom-images.git
+az vm availability-set create -g custom-images -n myset -l uksouth --platform-fault-domain-count 2 --platform-update-domain-count 5
+az vm create -g custom-images -n myVM -l uksouth --generate-ssh-key --availability-set myset --image UbuntuLTS --custom-data cloud-init.txt
+az vm open-port --resource group custom-images --name myVM --port 5000
 git push -u origin master
+
 
